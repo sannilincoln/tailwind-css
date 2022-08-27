ebbb9c1fa280dfdd03f6822c931687ba46c62201
sannilincoln <gbengaslc@gmail.com>
2022-08-27 09:59:11 +0100

    signUp/reg pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import Container from '@mui/material/Container';
 import Home from './Pages/Home';
+import Login from '../src/Pages/Login'
+import SignUp from './Pages/SignUp';
 
 
 
 export default function App() {
   return (
-     <Home/>
+     <SignUp/>
     // <Container maxWidth="sm">
     // </Container>
   );
